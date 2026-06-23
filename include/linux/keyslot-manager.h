c3337da0503ec3b83724df895e744fffbf0d1bb0
普通的玩家863 <liuweien339@gmail.com>
2026-06-23 19:23:53 +0800

    Update keyslot-manager.h

--- include/linux/keyslot-manager.h
+++ include/linux/keyslot-manager.h
@@ -98,7 +98,10 @@ int keyslot_manager_derive_raw_secret(struct keyslot_manager *ksm,
 				      unsigned int wrapped_key_size,
 				      u8 *secret, unsigned int secret_size);
 
-inline void ksm_flock(struct keyslot_manager *ksm, unsigned int flags);
+static inline void ksm_flock(struct keyslot_manager *ksm, unsigned int flags);
+{
+
+}
 
 #endif /* CONFIG_BLK_INLINE_ENCRYPTION */
 
